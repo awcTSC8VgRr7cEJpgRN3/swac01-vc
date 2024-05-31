44712cd90725a8f3d57c59c2716f253dea53e95b
awcTSC8VgRr7cEJpgRN3-local <90751388+awcTSC8VgRr7cEJpgRN3@users.noreply.github.com>
2024-06-01 02:03:22 +0800

    modify: display
    - Deduplicate modules.
    - Add called arguments in module parameters.

--- display5461AS1.sv
+++ display5461AS1.sv
@@ -5,18 +5,16 @@ module display5461AS1 (
     input [15:0] hexx, // 4 hex digits work for display 0 to 9, A, b, C, d, E, F
     input [3:0] points,
     output reg [7:0] segments, // a, b, c, d, e, f, g, dp
-    output reg [3:0] digits // D4, D3, D2, D1
+    output reg [3:0] digits // State D4, D3, D2, D1
 );
 
-wire [7:0] segs [3:0];
-hex27segs h27s0(hexx[3:0], segs[0][7:1]);
-hex27segs h27s1(hexx[7:4], segs[1][7:1]);
-hex27segs h27s2(hexx[11:8], segs[2][7:1]);
-hex27segs h27s3(hexx[15:12], segs[3][7:1]);
-assign segs[0][0] = points[0];
-assign segs[1][0] = points[1];
-assign segs[2][0] = points[2];
-assign segs[3][0] = points[3];
+reg [3:0] hex;
+reg [7:1] segs;
+hex27segs h27s(
+    .hex( hex ),
+    .segments( segs )
+);
+
 reg [14:0] pwm_counter;
 reg turn_on;
 
@@ -31,12 +29,13 @@ end
 always @(posedge clk) begin
     pwm_counter <= pwm_counter + 1;
     if (en && turn_on) begin
+        segments[7:1] <= segs;
         digits <= {digits[2:0], digits[3]};
         case (digits)
-            4'b0001: segments <= segs[1];
-            4'b0010: segments <= segs[2];
-            4'b0100: segments <= segs[3];
-            4'b1000: segments <= segs[0];
+            4'b0001: begin hex <= hexx[11:8]; segments[0] <= points[1]; end
+            4'b0010: begin hex <= hexx[15:12]; segments[0] <= points[2]; end
+            4'b0100: begin hex <= hexx[3:0]; segments[0] <= points[3]; end
+            4'b1000: begin hex <= hexx[7:4]; segments[0] <= points[0]; end
             default: digits <= 4'b1000;
         endcase
     end
--- swac01.v
+++ swac01.v
@@ -13,6 +13,14 @@ always @(posedge gclk) begin
     clk_counter <= clk_counter + 1;
 end
 
-display5461AS1 disp(clk_counter[7], 1'b1, 4'd11, 16'h3210, 4'b0100, segments, digits);
+display5461AS1 disp(
+    .clk( clk_counter[7] ),
+    .en( 1'b1 ),
+    .luminance( 4'd11 ),
+    .hexx( 16'h3210 ),
+    .points( 4'b0100 ),
+    .segments( segments ),
+    .digits( digits )
+);
 
 endmodule
