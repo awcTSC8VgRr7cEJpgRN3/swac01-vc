c9d9d2aec47170810baf0e61f3a12bd9e46aa375
awcTSC8VgRr7cEJpgRN3-local <90751388+awcTSC8VgRr7cEJpgRN3@users.noreply.github.com>
2024-06-01 18:08:46 +0800

    refactor: display
    - SystemVerilog is not necessary.

--- display5461AS1.v
+++ display5461AS1.v
@@ -5,20 +5,29 @@ module display5461AS1 (
     input [15:0] hexx, // 4 hex digits work for display 0 to 9, A, b, C, d, E, F
     input [3:0] points,
     output reg [7:0] segments, // a, b, c, d, e, f, g, dp
-    output reg [3:0] digits // D4, D3, D2, D1
+    output reg [3:0] digits // State D4, D3, D2, D1
 );
 
-wire [31:0] segs;
-hex27segs h27s0(hexx[3:0], segs[7:1]);
-hex27segs h27s1(hexx[7:4], segs[15:9]);
-hex27segs h27s2(hexx[11:8], segs[23:17]);
-hex27segs h27s3(hexx[15:12], segs[31:25]);
-assign segs[0] = points[0];
-assign segs[8] = points[1];
-assign segs[16] = points[2];
-assign segs[24] = points[3];
+reg [3:0] hex;
+wire [7:1] segs;
+hex27segs h27s(
+    .hex( hex ),
+    .segments( segs )
+);
+
+always @(*) begin
+    case (digits)
+        4'b0001: begin hex = hexx[3:0];   segments = { segs, points[0] }; end
+        4'b0010: begin hex = hexx[7:4];   segments = { segs, points[1] }; end
+        4'b0100: begin hex = hexx[11:8];  segments = { segs, points[2] }; end
+        4'b1000: begin hex = hexx[15:12]; segments = { segs, points[3] }; end
+        default: segments = 8'b0;
+    endcase
+end
+
 reg [14:0] pwm_counter;
 reg turn_on;
+reg [3:0] digits_next;
 
 integer i;
 always @(*) begin
@@ -31,16 +40,16 @@ end
 always @(posedge clk) begin
     pwm_counter <= pwm_counter + 1;
     if (en && turn_on) begin
-        digits <= {digits[2:0], digits[3]};
-        case (digits)
-            4'b0001: segments <= segs[15:8];
-            4'b0010: segments <= segs[23:16];
-            4'b0100: segments <= segs[31:24];
-            4'b1000: segments <= segs[7:0];
-            default: digits <= 4'b1000;
+        digits_next <= { digits_next[2:0], digits_next[3] };
+        case (digits_next)
+            4'b0001: digits <= digits_next;
+            4'b0010: digits <= digits_next;
+            4'b0100: digits <= digits_next;
+            4'b1000: digits <= digits_next;
+            default: digits_next <= 4'b0001;
         endcase
     end
-    else segments <= 8'b0;
+    else digits <= 4'b0;
 end
 
 endmodule
